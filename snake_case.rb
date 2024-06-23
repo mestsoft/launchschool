86c706f2eea0432363b70c25deb58151c791def6
Mestsoft <mestsoft@Mestsofts-MacBook-Pro.local>
2024-06-23 18:35:02 -0300

    innitial commit

---
+++ snake_case.rb
@@ -0,0 +1,37 @@
+# constant declaration
+FOUR = "Four"
+FIVE = 5
+
+forty_two = 42
+
+def this_is_great_method
+  # method
+end
+
+# multi-line
+[1,2,3].each do |i|
+  puts i
+end
+
+[4,5,6].each {|i| puts i}
+
+# class naming
+class MyFirstClass
+
+end
+
+class MySecondClass
+
+end
+
+puts "word wide web".split
+
+b = String.new("blue")
+puts b
+
+puts String.try_convert("red")
+
+puts "cat".between?("ant", "zebra")
+
+
+
